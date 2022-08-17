ee4895a55eaf89408dee97e56616e81fcd71f4c0
Peter <italycappuccino@gmail.com>
2022-08-17 12:12:15 +0800

    test

---
+++ src/test/java/com/stone/test/Food.java
@@ -0,0 +1,13 @@
+package com.stone.test;
+
+/**
+ * @author peter
+ * @description TODO
+ * @email italycappuccino@gmail.com
+ * @date 8/17/22 12:05 PM
+ */
+public class Food {
+    public static void main(String[] args) {
+        System.out.println("Food");
+    }
+}
